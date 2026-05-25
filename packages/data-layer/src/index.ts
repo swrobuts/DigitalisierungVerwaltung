0c8ad50e9a586e06d2612246b3481b22c2a6da07
swrobuts <swrobuts@googlemail.com>
2026-05-25 16:11:48 +0200

    feat(data-layer): index re-exports

--- packages/data-layer/src/index.ts
+++ packages/data-layer/src/index.ts
@@ -1,1 +1,5 @@
+export * from "./db-types";
 export { getSupabase, resetSupabaseClient, type SupabaseConfig } from "./supabase-client";
+export * from "./antraege";
+export * from "./helfer";
+export * from "./anlagen";
